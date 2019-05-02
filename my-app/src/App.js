5eac683e0f8899ff69d164460e88c1a541469783
Charles Renk <renkx008@umn.edu>
2019-05-02 15:58:51 -0500

    react error handling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -43,19 +43,12 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      paddinng: '8px',
-      cursor: 'pointer',
-    }
-
     let persons = null;
+    let btnClass = '';
      
     if (this.state.showPersons){
       persons = (
+
         <div>
         {this.state.persons.map((person, index) => {
           return <Person
@@ -69,27 +62,25 @@ class App extends Component {
         })}       
       </div>
       )
-      style.backgroundColor = 'red';
-
+        btnClass = classes.Red
     }
 
-    let classes = []
+   const assignedClasses = []
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1){
-    classes.push('bold')
+    assignedClasses.push(classes.bold)
     }
 
     //JSX
     return (
 
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button 
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button className={btnClass}
         onClick={this.togglePersonsHandler}
-        style={style}
         >Toggle Display</button>
         
         {persons}
